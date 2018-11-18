6e93ea2da66ccc2213809dc013f091a539cd23f6
Filipe Machado <filipe__machado@hotmail.com>
2018-11-18 16:36:10 -0200

    Update Menu
    Por hora, um menu mais coerente com o projeto.

--- main.c
+++ main.c
@@ -17,34 +17,74 @@
 			i=0;
 			printf("			#Arvore Binaria de Busca\n");
 			printf("\n");
-			printf("MENU DE OPCOES:\n");
-			printf("1)Mostrar Arvore.\n");
-			printf("2)Procurar Valor.\n");
-			printf("3)Inserir Valor.\n");
-			printf("10) Sair.\n");
-			fflush(stdin);
-			scanf("%d",&choice);
-			
-			switch(choice)
-				case 1:{
-					printf("Escolha uma Arvore Binaria:\n");
+			printf("\n");
+			printf("Escolha uma Arvore Binaria:\n");
 					for(i=1;i<7;i++){
 						printf("%d)Arvore Binaria %d\n",i,i);
 					}
-					scanf("%d",&choice2);					
-					get_filename(choice2,filename);
+			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 9);
+			printf("%d)Fechar o Programa;\n",i);
+			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
+			
+			
+			fflush(stdin);
+			scanf("%d",&choice);
+			
+			if(choice < 7){
+				do{
+					get_filename(choice,filename);
 					raiz = loadTreeFromFile(filename);
-					
-					break;
-				}
-			
-			
-		}while(choice!=10);
+					printf("\n");
+					printf("MENU DE OPCOES:\n");
+					if(choice2>10){
+					SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 4);
+					printf("Escolha algum valor valido!\n");
+					SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
+					}else{
+						printf("\n");
+					}
+					printf("1)Mostrar Arvore Atual;\n");
+					printf("2)Imprimir na tela se a arvore eh cheia;\n");
+					printf("3)Procurar Valor;\n");
+					printf("4)Imprimir na tela a altura da arvore;\n");
+					printf("5)Remover Valor;\n");
+					printf("6)Print In-Order;\n");
+					printf("7)Print Pre-Order;\n");
+					printf("8)Print Post-Order;\n");
+					printf("9)Balancear Arvore;\n");
+					SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 9);
+					printf("10) Voltar para o menu anterior;\n");
+					SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
+					printf("\n");
+					scanf("%d",&choice2);
+				
+					switch(choice2){
+							case 1:{					
+						
+							break;
+							}
+							case 10:{
+							break;
+							}
+					}
+				
+				}while(choice2 != 10);
+			}else if(choice>7){
+				SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 4);
+				printf("Escolha algum valor valido!\n");
+				SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
+			}
+		}while(choice!=7);
 		
 		printf("Fim do programa\n");
 		
 		return EXIT_SUCCESS;
 	}
+
+
+
+
+
 
 
 
